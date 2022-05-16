dab5884cc6a45cc87c08cd98e99049e9f67d0c7b
Dustin Le <ledustin2@gmail.com>
2022-05-16 16:44:39 -0500

    LeetCode #1302: Deepest Leaves Sum

--- mergeIntervals.java
+++ mergeIntervals.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /** 
- * LeetCode #56: Merge Intervals
+ * LeetCode #56: Merge Intervals (https://leetcode.com/problems/merge-intervals/)
  * Given an array of intervals where intervals[i] = [starti, endi], 
  * merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
  */
